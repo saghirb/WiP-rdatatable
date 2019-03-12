928a1acd546d531ffbfb9e2ffcc65b1cd6bf03fb
Saghir Bashir <saghir@ilustat.com>
2019-03-12 18:06:18 +0000

    First project commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # WiP-rdatatable
 Women in Parliament: R 'data.table' Case Study
+
+**README.md to be finalised**
+
+- To do:
+    + Add Contibutors.md
+    + Prepare Exercise Rmd file
+
+## Requirements
+- Link to:
+    + [Getting Started in R - Tinyverse addition](https://github.com/eddelbuettel/gsir-te)
+    + [Tinyverse](http://www.tinyverse.org/)
+    + ggplot2 knowledge
+  
+
+
+## Objective of Case Studies 
+
+- Demonstrate key features of the `data.table` package using *real-life* data.
+- Explore the data to answer relevant and pertinent questions.
+- Show how things are done whilst keeping the technical details light.
+- Explain our motivation and thinking processes.
+
+
+# `data.table`
+
+For more information see [http://r-datatable.com](http://r-datatable.com)
+
+## Acknowledgements
+
+Thanks to [Matt Dowle](https://github.com/mattdowle), [Arun Srinivasan](https://github.com/arunsrinivasan) and all the `data.table` [team](https://github.com/orgs/Rdatatable/people) and contributors for such a great package.
+
+
+
---
+++ _make.R
@@ -0,0 +1,39 @@
+## Run all files to prepare "Women in Parliament -- data.table"
+
+# Setup
+library(here)
+here()
+
+# Download the zipped data file and extract the files to the "data/" directory.
+# First empty the "share"data/ folder and recreate the directory structure.
+unlink(here("data/*"), recursive = TRUE)
+
+tmpZip <- tempfile()
+download.file("http://api.worldbank.org/v2/en/indicator/SG.GEN.PARL.ZS?downloadformat=csv",
+              destfile = tmpZip)
+unzip(tmpZip, exdir = here("data"))
+
+WiPDataFile <- list.files(path = here("data"), pattern = "^API")
+file.copy(here("data", WiPDataFile), here("data", "WB-WiP.csv"))
+
+# Render the guide and produce the zip file for distribution.
+rmarkdown::render(here("doc", "WiP-rdatatable.Rmd"))
+
+# Create zip files to share with participants
+# First empty the share folder and recreate the directory structure.
+unlink(here("Share/*"), recursive = TRUE)
+dir.create(here("Share", "data"))
+
+# Populate the Share directories
+file.copy(here("doc", "WiP-rdatatable.pdf"), here("Share"))
+file.copy(here("data", "WB-WiP.csv"), here("Share", "data"))
+
+# Creating (initialising) an RStudio project
+rstudioapi::initializeProject(path = here("Share"))
+file.rename(here("Share", "Share.Rproj"), here("Share", "WiP-dt.Rproj"))
+
+# Using here() function with zip results in full paths in the zip files :(
+# Not beautiful: Using setwd to overcome the full paths issue above.
+setwd(here("Share"))
+zip(here("Share", "WiP-rdatatable.zip"), "./", extras = "-FS")
+setwd(here())
